baa66d4b9b4e54cc0c985ec7b1310fc5f7e756f7
Chris Bowen <info@cloud-wales.co.uk>
2021-06-09 21:08:32 +0100

    Refactored to helper class

--- Helper.py
+++ Helper.py
@@ -2,9 +2,11 @@
 
 import pyperclip
 
-def lowercase(sentence):
-    pyperclip.copy(sentence.lower())
-    print("\n\033[1;33;40mOutput:\033[0;0m")
-    print(sentence.lower())
-    pyperclip.copy(sentence.lower())
-    print("\n\033[3;32;40mCopied to clipboard... \033[0;0m\n")
+class Helper():
+
+    def convert_to_lowercase(sentence):
+        pyperclip.copy(sentence.lower())
+        print("\n\033[1;33;40mOutput:\033[0;0m")
+        print(sentence.lower())
+        pyperclip.copy(sentence.lower())
+        print("\n\033[3;32;40mCopied to clipboard... \033[0;0m\n")
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from Helper import *
 
 while True:
 	sentence = input("\n\033[1;33;40mInput:\033[0;0m\n")
-	lowercase(sentence)
+
+	Helper.convert_to_lowercase(sentence)
 
 	if input("Would you like to convert another sentance? [y]yes or [n]no \n").lower() not in ('y', 'yes'):
 		break
